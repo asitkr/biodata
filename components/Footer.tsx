d8be68106bd5bb29fc94b041584d0b2bad2d8139
asitkr <krsinhasaurabh@gmail.com>
2025-12-14 11:35:49 +0530

    footer scroll modified

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,16 @@ const Footer = () => {
     return (
         <footer className="py-8 bg-slate-50 dark:bg-slate-950 text-center border-t border-slate-200 dark:border-slate-800 transition-colors duration-300">
             <p className="text-slate-500 dark:text-slate-400">
-                &#169; {new Date().getFullYear()} <span className="text-blue-500">Ashit</span> | Biodata. All rights reserved.
+                &#169; {new Date().getFullYear()}  <a 
+            href="#hero" 
+            onClick={(e) => {
+              e.preventDefault();
+              document.getElementById('hero')?.scrollIntoView({ behavior: 'smooth' });
+            }}
+            className={`font-semibold cursor-pointer transition-colors duration-300 dark:text-blue-400 dark:hover:text-blue-300 text-blue-500 hover:text-blue-600`}
+          >
+            Ashit
+          </a> | Biodata. All rights reserved.
             </p>
         </footer>
     )
